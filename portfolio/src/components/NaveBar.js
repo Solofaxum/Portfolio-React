069b19542f56afde4566c4362ad696c250d24ca1
Solofaxum <solofaxum6@gmail.com>
2021-02-15 18:27:54 -0800

    navigation completed

--- portfolio/src/components/NaveBar.js
+++ portfolio/src/components/NaveBar.js
@@ -20,6 +20,9 @@ export default function NaveBar() {
                         activeClassName="text-red-100 bg-red-700"
                         className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover text-red-200 hover:text-green-800"> About me</NavLink>
                 </nav>
+                <div>
+                    
+                </div>
             </div>
         </header>
     )
