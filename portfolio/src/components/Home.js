6f9804f8293583cde686a2373b7c070aef53c99b
Solofaxum <solofaxum6@gmail.com>
2021-02-18 15:13:22 -0800

    social icons are added and connected

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,5 +1,14 @@
 import React from "react";
+import image from "../home.jpeg";
+
 
 export default function Home(){
-    return <h1>Home page!</h1>
+    return (
+        <main>
+            <img src={image} alt="sky stars" className="absolute object-cover w-full h-full"/>
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name ">Hi I'm Solomon.</h1>
+            </section>
+        </main>
+    )
 }
--- portfolio/src/components/NaveBar.js
+++ portfolio/src/components/NaveBar.js
@@ -1,5 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import './icons.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faYoutube,
+    faFacebook,
+    faTwitter,
+    faInstagram
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function NaveBar() {
     return (
@@ -20,8 +28,28 @@ export default function NaveBar() {
                         activeClassName="text-red-100 bg-red-700"
                         className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover text-red-200 hover:text-green-800"> About me</NavLink>
                 </nav>
-                <div>
-                    
+                <div className="social-container">
+                    <h3>Social Follow</h3>
+                    <a href="https://www.youtube.com/channel/UCNm0ivFgx2ELjb51kD3FSEw"
+                        className="youtube social color: #eb3223">
+                        <FontAwesomeIcon icon={faYoutube} size="2x" />
+                    </a>
+                    <a href="https://www.facebook.com/Solomon.axu/"
+                        className="facebook social color: #4968ad">
+                        <FontAwesomeIcon icon={faFacebook} size="2x" />
+                    </a>
+                    <a href="https://twitter.com/solomong_slasie" className="twitter social">
+                        <FontAwesomeIcon icon={faTwitter} size="2x" />
+                    </a>
+                    <a href="https://github.com/Solofaxum"
+                        className="instagram social">
+                        <FontAwesomeIcon icon={faGithub} size="2x" />
+                    </a>
+                    <a href="https://www.instagram.com/learnbuildteach"
+                        className="instagram social">
+                        <FontAwesomeIcon icon={faInstagram} size="2x" />
+                    </a>
+
                 </div>
             </div>
         </header>
